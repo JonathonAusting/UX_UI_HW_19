2bb7233eb2488f6035f3d5b457fb9127b777b347
JonathonAusting <jon.austing@gmail.com>
2021-07-15 12:12:34 -0400

    Added icons to the skills section and reorganized the work section.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(window).scroll( function(){
         $('.fade').each( function(i){
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_element = $(this).offset().top + $(this).height();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_element ){
                 $(this).animate({'opacity':'1'},1000);
